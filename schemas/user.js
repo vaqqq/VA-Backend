2f0f60fbc3188ae347802a12d253b13f89f91638
vaq <maxmari395@gmail.com>
2025-01-26 16:12:20 +0100

    fix: add password field to CompanySchema employees

--- schemas/user.js
+++ schemas/user.js
@@ -21,6 +21,7 @@ const CompanySchema = new mongoose.Schema({
     employees: [
         {
             name: { type: String, required: true },
+            password: { type: String, required: true },
             role: { type: String, default: 'employee' },
             createdAt: { type: Date, default: Date.now },
         },
